a0f26dd25e9dbc807dd2e2360acdd49fc79b092f
Jeddaa <fathiato@gmail.com>
2022-05-20 14:04:15 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_abs - prints the absolute value of a number
+ *@n: character to be cased
+ *Return: 0
+ */
+
+int _abs(int n)
+{
+if (n >= 1)
+return (n);
+else if (n <= 0)
+return (n * -1);
+else
+return (0);
+}
