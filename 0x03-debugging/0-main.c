6772c36d3744c89ee9ab76ff445685b93d5bd446
Jeddaa <fathiat0@gmail.com>
2022-05-24 00:49:36 +0100

    task 3

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests if an integer is positive or negative
+ * 
+ * Return: always 0
+ */
+
+int main (void)
+{
+    int a = -345;
+    positive_or_negative(a);
+
+    return (0);
+}
---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * positive_or_negative - prints if an integer is
+ * positive or negative
+ * @i: integer to be tested.
+ * Returns: always 0
+ */
+
+void positive_or_negative(int i)
+{
+    if (i > 0)
+    {
+        printf("%d is positive.\n", i);
+    }
+    else if (i < 0)
+    {
+        printf("%d is negative.\n", i);
+    }
+    else
+    { 
+        printf("%d is zero.\n", i);
+    }
+}
