0449ab0ed08a75d1836b3aef074162afa4b19248
Jeddaa <fathiat0@gmail.com>
2022-05-24 14:01:25 +0100

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * @brief 
+ * 
+ */
+
+void print_line(int n)
+{
+   int i = 1;
+    while (i <= n)
+    {
+        _putchar('_');
+        i++;
+    }
+    _putchar('\n');
+}
