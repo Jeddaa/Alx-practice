6d81cc0bb2e704673d1de60f2cecc22feb78b85e
Jeddaa <fathiat0@gmail.com>
2022-05-26 15:10:41 +0100

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * @brief 
+ * 
+ */
+
+void puts_half(char *str)
+{
+    int i, length = 0;
+    for (i = 0; *(str + i) != '\0'; i++)
+    {
+        length++;
+    }
+    if ((length % 2) == 0)
+    {
+        for (i = 0; i < length; i++)
+        {
+            _putchar(*(str + i));
+        }
+    }
+    else if ((length % 2) != 0)
+    {
+        int len = ((length - 1) / 2);
+        for (i = len + 1; i < length; i++)
+        {
+            _putchar(*(str + i));
+        }
+    }
+    _putchar('\0');
+}
