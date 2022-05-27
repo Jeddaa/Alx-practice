c9f2bfd3dc2d0c4171546eeb3c745cbb28bbd93f
Jeddaa <fathiat0@gmail.com>
2022-05-27 12:29:29 +0100

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * @brief 
+ * 
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+    char i;
+    for (i = 0; src[i] != '\0'; i++)
+    {
+        dest[i] = src[i];
+    }
+    dest[i] != '\0';
+    return (dest);
+}
